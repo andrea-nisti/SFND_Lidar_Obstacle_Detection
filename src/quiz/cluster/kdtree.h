c3619cbce93fa816c353afd342c54f2e403c6b2e
Andrea Nisticò <andrea.nistic@gmail.com>
2022-10-13 17:35:43 +0200

    Add search and refactor

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -2,6 +2,7 @@
 // Quiz on implementing kd tree
 
 #include "../../render/render.h"
+#include <cmath>
 
 // Structure to represent node of kd tree
 struct Node
@@ -64,30 +65,9 @@ bool setChild(Node *node, Node *const child, NodeSide side)
 		return false;
 		break;
 	}
+	return true;
 }
 
-void insertHelper(Node *&target, Node *const new_node, int depth = 0)
-{
-	if (target == nullptr)
-	{
-		target = new_node;
-	}
-	else
-	{
-		auto dim{target->point.size()};
-		auto index{depth % dim};
-		if ( new_node->point.at(index) < target->point.at(index))
-		{
-			target = target->left;
-			insertHelper(target, new_node, ++depth);
-		}
-		else
-		{
-			target = target->right;
-			insertHelper(target, new_node, ++depth);
-		}
-	}
-}
 struct KdTree
 {
 	Node *root;
@@ -102,9 +82,68 @@ struct KdTree
 		delete root;
 	}
 
+	// Helpers
+	static void insertHelper(Node *&target, Node *const new_node, int depth = 0)
+	{
+		if (target == nullptr)
+		{
+			target = new_node;
+		}
+		else
+		{
+			auto dim{target->point.size()};
+			auto index{depth % dim};
+			if (new_node->point.at(index) < target->point.at(index))
+			{
+				insertHelper(target->left, new_node, ++depth);
+			}
+			else
+			{
+				insertHelper(target->right, new_node, ++depth);
+			}
+		}
+	}
+
+	static void searchHelper(std::vector<float> target, Node const *const node, float distanceTol, std::vector<int> &ids, int depth = 0)
+	{
+		if (node != nullptr)
+		{
+			auto di{std::vector<float>(target.size(), 0)};
+			size_t idx{0};
+			for (auto &component : di)
+			{
+				component = fabs(node->point.at(idx) - target.at(idx));
+			}
+
+			auto is_in_box{std::all_of(di.cbegin(), di.cend(), [distanceTol](float comp)
+									   {return comp <= distanceTol; })};
+
+			if (is_in_box)
+			{
+				// compute distance : e.g. sqrt(x*x + y*y + z*z)
+				float d{sqrtf(std::accumulate(di.begin(), di.end(), 0, [](float accumulator, float comp)
+												   {return accumulator + comp *comp; }))};
+				if (d <= distanceTol)
+				{
+					ids.push_back((node->id));
+				}
+			}
+
+			if ((target.at(depth % target.size()) + distanceTol) > node->point.at(depth % target.size()))
+			{
+				searchHelper(target, node->right, distanceTol, ids, ++depth);
+			}
+
+			if ((target.at(depth % target.size()) - distanceTol) < node->point.at(depth % target.size()))
+			{
+				searchHelper(target, node->left, distanceTol, ids, ++depth);
+			}
+		}
+	}
+
 	void insert(std::vector<float> point, int id)
 	{
-		auto new_node{makeNode(point, id)};\
+		auto new_node{makeNode(point, id)};
 		auto target{root};
 		if (root == nullptr)
 		{
@@ -118,6 +157,7 @@ struct KdTree
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		searchHelper(target, root, distanceTol, ids);
 		return ids;
 	}
 };
